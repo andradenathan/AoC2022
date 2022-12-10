1f5697882196fe9cbdb111098b1d152165b2246e
Nathan Andrade <nathan.nhdsk@gmail.com>
2022-12-10 02:01:51 -0300

    day1_2 done

--- Day1.java
+++ Day1.java
@@ -1,32 +1,46 @@
 import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static java.lang.Integer.compare;
 import static java.lang.Integer.parseInt;
 
 public class Day1 {
     public static void main(String[] args) throws FileNotFoundException {
-        getDataFromFile("./day1_input.txt");
+        calculate("./day1_input.txt");
     }
 
-    private static void getDataFromFile(String path) throws FileNotFoundException {
-        Integer totalCalories = 0;
+
+    private static void calculate(String path) throws FileNotFoundException {
         try {
             String data;
-            Integer calories = 0;
+            int calories = 0;
+            int index = 0;
 
             File file = new File(path);
             BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            Map<Integer, Integer> elvesMap = new HashMap<>();
 
             while((data = bufferedReader.readLine()) != null) {
                 if(data.isEmpty()) {
-                    if(calories > totalCalories) {
-                        totalCalories = calories;
-                    }
+                    elvesMap.put(index, calories);
+                    index++;
                     calories = 0;
                 } else {
                     calories += parseInt(data);
                 }
             }
-            System.out.println(totalCalories);
+
+            Integer result = elvesMap
+                    .entrySet()
+                    .stream()
+                    .sorted((c1, c2) -> c2.getValue() - c1.getValue())
+                    .toList()
+                    .subList(0, 3)
+                    .stream()
+                    .mapToInt(Map.Entry::getValue).sum();
+
+            System.out.println(result);
         } catch (FileNotFoundException fileNotFoundException) {
             throw new FileNotFoundException("File not found error: " + fileNotFoundException);
         } catch (IOException ioException) {
